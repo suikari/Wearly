c67316d7bb21d318affba901d681d4f2e34b5c3d
codingjs12 <202245068@itc.ac.kr>
2025-06-13 17:11:40 +0900

    250613 글쓰기(제목, 내용, 태그, 느낌, 공개)

--- lib/main/write_post_page.dart
+++ lib/main/write_post_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:image_editor_plus/image_editor_plus.dart';
@@ -15,27 +16,32 @@ class WritePostPage extends StatefulWidget {
 }
 
 class _WritePostPageState extends State<WritePostPage> {
+
+  FirebaseFirestore fs = FirebaseFirestore.instance;
+
+
+
   final TextEditingController titleController = TextEditingController();
   final TextEditingController contentController = TextEditingController();
-
+  final List<String> categories = ['#상의', '#하의', '#아우터', '#기타', '#분위기'];
   String selectedWeather = '맑음';
   String selectedTemperature = '20℃';
+  String selectedFeeling = '적당해요';
   bool isPublic = true;
 
-  final List<String> tags = [
-    '#간편복', '#긴팔', '#셔츠/블라우스', '#조끼', '#청바지', '#트레이닝', '#숏팬츠',
-    '#레깅스', '#자켓', '#가디건', '#패딩', '#코트', '#원피스/치마', '#기모', '#모자/악세서리'
-  ];
-  final Set<String> selectedTags = {};
+  final Map<String, List<String>> categoryTags = {
+    '#상의': ['#민소매/반팔', '#긴팔', '#셔츠/블라우스', '#스웨터/니트', '#맨투맨/후드'],
+    '#하의': ['#청바지', '#면바지', '#슬렉스', '#반바지'],
+    '#아우터': ['#자켓', '#가디건', '#패딩', '#코트', '#바람막이', '#집업'],
+    '#기타': ['#내의/스타킹', '#기모', '#모자/목도리'],
+    '#분위기': ['#스트릿', '#캐쥬얼', '#미니멀', '#빈티지', '#시크', '#클래식', '#보헤미안', '#스포티']
+  };
+
+  List<String> selectedTags = [];
+
   final List<File> selectedImages = [];
   final int maxImageCount = 10;
   int currentPageIndex = 0;
-
-  void toggleTag(String tag) {
-    setState(() {
-      selectedTags.contains(tag) ? selectedTags.remove(tag) : selectedTags.add(tag);
-    });
-  }
 
   Future<void> pickAndEditImages() async {
     if (selectedImages.length >= maxImageCount) {
@@ -84,14 +90,17 @@ class _WritePostPageState extends State<WritePostPage> {
   }
 
   void resetForm() {
-    titleController.clear();
-    contentController.clear();
-    selectedTags.clear();
-    selectedImages.clear();
-    selectedWeather = '맑음';
-    selectedTemperature = '20℃';
-    isPublic = true;
-    currentPageIndex = 0;
+    setState(() {
+      titleController.clear();
+      contentController.clear();
+      selectedTags.clear();
+      selectedImages.clear();
+      selectedWeather = '맑음';
+      selectedTemperature = '20℃';
+      isPublic = true;
+      currentPageIndex = 0;
+    });
+
   }
 
   @override
@@ -110,7 +119,6 @@ class _WritePostPageState extends State<WritePostPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // 이미지 슬라이더 영역
             Container(
               height: 500,
               decoration: BoxDecoration(
@@ -118,73 +126,57 @@ class _WritePostPageState extends State<WritePostPage> {
                 borderRadius: BorderRadius.circular(16),
               ),
               child: selectedImages.isEmpty
-                  ? Center(
-                child: Text(
-                  '이미지를 선택해주세요.',
-                  style: TextStyle(color: Colors.grey),
-                ),
-              )
+                  ? Center(child: Text('이미지를 선택해주세요.', style: TextStyle(color: Colors.grey)))
                   : Column(
                 children: [
                   Expanded(
                     child: PageView.builder(
                       itemCount: selectedImages.length,
-                      onPageChanged: (index) {
-                        setState(() => currentPageIndex = index);
-                      },
-                      itemBuilder: (context, index) {
-                        return Stack(
-                          children: [
-                            SizedBox.expand(
-                              child: Image.file(
-                                selectedImages[index],
-                                fit: BoxFit.fill,
-                              ),
-                            ),
-                            Positioned(
-                              top: 8,
-                              right: 8,
-                              child: GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    selectedImages.removeAt(index);
-                                    if (currentPageIndex >= selectedImages.length &&
-                                        currentPageIndex > 0) {
-                                      currentPageIndex--;
-                                    }
-                                  });
-                                },
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.black45,
-                                    shape: BoxShape.circle,
-                                  ),
-                                  padding: const EdgeInsets.all(6),
-                                  child:
-                                  const Icon(Icons.close, color: Colors.white),
+                      onPageChanged: (index) => setState(() => currentPageIndex = index),
+                      itemBuilder: (context, index) => Stack(
+                        children: [
+                          SizedBox.expand(
+                            child: Image.file(selectedImages[index], fit: BoxFit.fill),
+                          ),
+                          Positioned(
+                            top: 8,
+                            right: 8,
+                            child: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  selectedImages.removeAt(index);
+                                  if (currentPageIndex >= selectedImages.length && currentPageIndex > 0) {
+                                    currentPageIndex--;
+                                  }
+                                });
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.black45,
+                                  shape: BoxShape.circle,
                                 ),
+                                padding: const EdgeInsets.all(6),
+                                child: const Icon(Icons.close, color: Colors.white),
                               ),
                             ),
-                          ],
-                        );
-                      },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   const SizedBox(height: 8),
                   if (selectedImages.length > 1)
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: List.generate(selectedImages.length, (index) {
-                        return Container(
-                          width: 8,
-                          height: 8,
-                          margin: const EdgeInsets.symmetric(horizontal: 4),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: currentPageIndex == index ? Colors.pink : Colors.grey,
-                          ),
-                        );
-                      }),
+                      children: List.generate(selectedImages.length, (index) => Container(
+                        width: 8,
+                        height: 8,
+                        margin: const EdgeInsets.symmetric(horizontal: 4),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: currentPageIndex == index ? Colors.pink : Colors.grey,
+                        ),
+                      )),
                     ),
                   const SizedBox(height: 8),
                 ],
@@ -201,51 +193,58 @@ class _WritePostPageState extends State<WritePostPage> {
               ),
             ),
             const SizedBox(height: 16),
-
-            // 제목 입력
             TextField(
               controller: titleController,
               decoration: InputDecoration(
                 hintText: '제목을 입력해주세요...',
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.pink.shade200),
-                ),
+                border: OutlineInputBorder(borderSide: BorderSide(color: Colors.pink.shade200)),
               ),
             ),
             const SizedBox(height: 12),
-
-            // 내용 입력
             TextField(
               controller: contentController,
               maxLines: 5,
               decoration: InputDecoration(
                 hintText: '내용을 입력해주세요...',
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.pink.shade200),
-                ),
+                border: OutlineInputBorder(borderSide: BorderSide(color: Colors.pink.shade200)),
               ),
             ),
             const SizedBox(height: 20),
-
-            // 태그 선택
-            const Text('태그', style: TextStyle(fontWeight: FontWeight.bold)),
-            const SizedBox(height: 8),
-            Wrap(
-              spacing: 8,
-              runSpacing: 8,
-              children: tags.map((tag) {
-                final isSelected = selectedTags.contains(tag);
-                return ChoiceChip(
-                  label: Text(tag),
-                  selected: isSelected,
-                  onSelected: (_) => toggleTag(tag),
-                  selectedColor: Colors.pink.shade100,
-                );
-              }).toList(),
-            ),
-            const SizedBox(height: 24),
-
-            // 날씨, 온도, 공개여부 선택
+            for (var category in categories) ...[
+              Text(category, style: const TextStyle(fontWeight: FontWeight.bold)),
+              const SizedBox(height: 8),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: categoryTags[category]!.map((tag) {
+                    final isSelected = selectedTags.contains(tag);
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: ChoiceChip(
+                        label: Text(tag),
+                        selected: isSelected,
+                        onSelected: (selected) {
+                          setState(() {
+                            if (selected) {
+                              selectedTags.add(tag);
+                            } else {
+                              selectedTags.remove(tag);
+                            }
+                          });
+                        },
+                        selectedColor: Colors.pink.shade100,
+                        backgroundColor: Colors.grey.shade200,
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(color: Colors.pink.shade200),
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+              const SizedBox(height: 16),
+            ],
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -261,6 +260,17 @@ class _WritePostPageState extends State<WritePostPage> {
                   ),
                 ]),
                 Row(children: [
+                  const Icon(Icons.wb_sunny_outlined),
+                  const SizedBox(width: 8),
+                  DropdownButton<String>(
+                    value: selectedFeeling,
+                    items: ['적당해요', '추웠어요', '더웠어요']
+                        .map((w) => DropdownMenuItem(value: w, child: Text(w)))
+                        .toList(),
+                    onChanged: (val) => setState(() => selectedFeeling = val!),
+                  ),
+                ]),
+                Row(children: [
                   DropdownButton<String>(
                     value: selectedTemperature,
                     items: ['10℃', '15℃', '20℃', '25℃', '30℃']
@@ -268,7 +278,7 @@ class _WritePostPageState extends State<WritePostPage> {
                         .toList(),
                     onChanged: (val) => setState(() => selectedTemperature = val!),
                   ),
-                  const SizedBox(width: 16),
+                  const SizedBox(width: 8),
                   Row(
                     children: [
                       const Text('공개 여부: '),
@@ -283,8 +293,6 @@ class _WritePostPageState extends State<WritePostPage> {
               ],
             ),
             const SizedBox(height: 24),
-
-            // 취소/등록 버튼
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -300,15 +308,44 @@ class _WritePostPageState extends State<WritePostPage> {
                   child: const Text('취소'),
                 ),
                 ElevatedButton(
-                  onPressed: () {
-                    // 업로드 로직 자리
+                  onPressed: () async {
                     debugPrint("제목: ${titleController.text}");
                     debugPrint("내용: ${contentController.text}");
-                    debugPrint("태그: ${selectedTags.toList()}");
+                    debugPrint("태그: ${selectedTags.toString()}");
                     debugPrint("날씨: $selectedWeather");
                     debugPrint("온도: $selectedTemperature");
                     debugPrint("공개: $isPublic");
+                    debugPrint("느낌: $selectedFeeling");
                     debugPrint("이미지 수: ${selectedImages.length}");
+
+                    if (titleController.text.trim().isEmpty && contentController.text.trim().isEmpty) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('제목과 내용을 모두 입력해주세요.'),
+                          backgroundColor: Colors.redAccent,
+                          duration: Duration(seconds: 2),
+                        ),
+                      );
+                      return; // 이후 코드 실행 방지
+                    }
+                    await fs.collection("feeds").add({
+                      "title" : titleController.text,
+                      "content" : contentController.text,
+                      "cdatetime" : Timestamp.now(),
+                      "isPublic" : isPublic,
+                      "temperature" : selectedTemperature,
+                      "feeling" : selectedFeeling
+                    });
+
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('등록되었습니다.'),
+                        backgroundColor: Colors.green,
+                        duration: Duration(seconds: 2),
+                      ),
+                    );
+                    resetForm();
+
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.pink,
