41fc0ebea71d78d9daaf9d18fe4a190d507f7a1f
codingjs12 <202245068@itc.ac.kr>
2025-06-17 11:34:11 +0900

    250617 AndroidManifest 수정 / 버전 수정

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -8,15 +8,8 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   firebase_auth
   firebase_core
-
-  geolocator_windows
-
-
   firebase_storage
-
   geolocator_windows
-
-
   permission_handler_windows
 )
 
